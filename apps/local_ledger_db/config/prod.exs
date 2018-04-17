987602dc17f8c7313e41e81a1076c6a3e74cf24b
Kridsada Thanabulpong <sirn@omise.co>
2018-04-17 23:52:28 +0700

    Update url_dispatcher and local_ledger_db to use DeferredConfig.

--- apps/local_ledger_db/config/prod.exs
+++ apps/local_ledger_db/config/prod.exs
@@ -2,11 +2,15 @@ use Mix.Config
 
 config :local_ledger_db, LocalLedgerDB.Repo,
   adapter: Ecto.Adapters.Postgres,
-  url: System.get_env("LOCAL_LEDGER_DATABASE_URL") || "postgres://localhost/local_ledger_prod"
-
-key = System.get_env("CAISHEN_SECRET_KEY") || System.get_env("LOCAL_LEDGER_SECRET_KEY")
+  url: {:system, "LOCAL_LEDGER_DATABASE_URL", "postgres://localhost/local_ledger_prod"}
 
 config :cloak, Salty.SecretBox.Cloak,
-       tag: "SBX",
-       default: true,
-       keys: [%{tag: <<1>>, key: key, default: true}]
+  tag: "SBX",
+  default: true,
+  keys: [
+    %{
+      tag: <<1>>,
+      key: {:system, "LOCAL_LEDGER_SECRET_KEY"},
+      default: true
+    }
+  ]
