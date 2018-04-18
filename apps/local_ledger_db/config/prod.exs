6d4855efba09bf768459d35b30e46baef5f643ee
Kridsada Thanabulpong <sirn@omise.co>
2018-04-18 21:18:02 +0700

    Use REPLACE_OS_VARS in Cloak.

--- apps/local_ledger_db/config/prod.exs
+++ apps/local_ledger_db/config/prod.exs
@@ -4,13 +4,15 @@ config :local_ledger_db, LocalLedgerDB.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: {:system, "LOCAL_LEDGER_DATABASE_URL", "postgres://localhost/local_ledger_prod"}
 
+# Cloak doesn't support {:system, ...}-style configuration so we
+# have to set REPLACE_OS_VARS=yes when running the app release.
 config :cloak, Salty.SecretBox.Cloak,
   tag: "SBX",
   default: true,
   keys: [
     %{
       tag: <<1>>,
-      key: {:system, "LOCAL_LEDGER_SECRET_KEY"},
+      key: "${LOCAL_LEDGER_SECRET_KEY}",
       default: true
     }
   ]
