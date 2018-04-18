cbbd7aea2f2380d06f1a39238c05d3ac8da40805
Kridsada Thanabulpong <sirn@omise.co>
2018-04-18 18:17:53 +0700

    Use MFA-style configuration in LocalLedger config.

---
+++ apps/local_ledger/lib/local_ledger/config.ex
@@ -0,0 +1,8 @@
+defmodule LocalLedger.Config do
+  def read_scheduler_config do
+    case System.get_env("BALANCE_CACHING_FREQUENCY") do
+      nil -> []
+      frequency -> [{frequency, {LocalLedger.CachedBalance, :cache_all, []}}]
+    end
+  end
+end
