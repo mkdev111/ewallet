bb349d10b3a5f03928020a62d6cc175e10ea40e5
Kridsada Thanabulpong <sirn@omise.co>
2018-04-24 00:03:04 +0700

    Properly set start_with_no_watch.

--- apps/url_dispatcher/config/prod.exs
+++ apps/url_dispatcher/config/prod.exs
@@ -3,5 +3,7 @@ use Mix.Config
 config :url_dispatcher,
   ecto_repos: [],
   serve_endpoints: true,
-  start_with_no_watch: true,
   port: {:system, "PORT", 4000}
+
+config :admin_panel,
+  start_with_no_watch: true
